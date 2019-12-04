a7323944a2d139bbff52d03b5076c087c36b5ca9
jiuweilinghu <1354895960@qq.com>
2019-12-04 15:40:16 +0800

    实现启发式算法的主体框架

--- src/HA.java
+++ src/HA.java
@@ -19,7 +19,26 @@ public class HA {
 
     // 启发式算法
     void heuristicAlgorithm(){
+        // 初始化平衡任务响应时间
+        initBalancedTime();
+        // 根据balancedTime将微云划分为两个集合
+        partition(cloudlet);
+        double blnTime=Double.MAX_VALUE; // 用于判断循环结束的条件
 
+        while(Math.abs(balancedTime-blnTime)>accuracy){
+            // 计算迁入迁出需求
+            calDemand();
+            // 计算最小延迟流
+            minLatencyFlow();
+            double maxTmp=0;
+            for(int j=0;j<underloadedSet.size();j++){
+                if(underloadedSet.get(j).taskResTime>maxTmp){
+                    maxTmp=underloadedSet.get(j).taskResTime;
+                }
+            }
+            blnTime=maxTmp;
+            balancedTime=(balancedTime+blnTime)/2;
+        }
     }
 
 
@@ -80,6 +99,14 @@ public class HA {
 
     // 最小延迟流
     void minLatencyFlow(){
+        // 构造最小费用最大流问题的网络
+
+        // 调用最小费用最大流算法求解
+        minCostMaxFlow();
+    }
+
+    // 最小费用最大流算法
+    void minCostMaxFlow(){
 
     }
 }
